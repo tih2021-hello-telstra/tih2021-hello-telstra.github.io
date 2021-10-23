ee7c230fd706d686478e549151af2537ba8e2b77
Cheng Chang <cheng.chang@team.telstra.com>
2021-10-23 14:19:38 +1100

    copy Troika ui example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import UIExample from './ui/UIExample'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <UIExample width="800" height="600" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
